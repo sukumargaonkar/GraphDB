816f15436734debe926b0171e386d3f73f29cd8c
Amogh Kulkarni <amogh.kulkarni1993@gmail.com>
2019-04-21 13:42:32 -0400

    Added JsonAgent for String to Json conversion and back

---
+++ src/main/java/com/graphdb/agent/JsonAgent.java
@@ -0,0 +1,22 @@
+package com.graphdb.agent;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class JsonAgent<T> {
+
+	private static Gson gson;
+
+	public JsonAgent() {
+		gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+	}
+
+	public String toJson(Object json) {
+		return gson.toJson(json);
+	}
+
+	public T fromJson(String json, Class<T> classOfT) {
+		return gson.fromJson(json, classOfT);
+	}
+
+}
