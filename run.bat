0c92b1a0e86653592044b3aac7b723d8acd7927e
Amogh Kulkarni <amogh.kulkarni1993@gmail.com>
2019-04-21 21:24:25 -0400

    added bat file to run simultaneous instances of nodes

---
+++ run.bat
@@ -0,0 +1,4 @@
+start cmd.exe /k "java -jar .\target\graphdb.jar member1 8080"
+start cmd.exe /k "java -jar .\target\graphdb.jar member2 8081"
+start cmd.exe /k "java -jar .\target\graphdb.jar member3 8082"
+start cmd.exe /k "java -jar .\target\graphdb.jar member4 8083"
